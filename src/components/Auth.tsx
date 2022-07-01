d8eb70d35265fd330ad0d883c721e0ddb5f0f2b0
shadeemerhi <shadmerhi@gmail.com>
2022-07-01 14:53:08 -0600

    add auth username flow

---
+++ src/components/Auth.tsx
@@ -0,0 +1,75 @@
+import axios from "axios";
+import { Session } from "next-auth";
+import { signIn } from "next-auth/react";
+import React, { useState } from "react";
+
+interface AuthProps {
+  session: Session | null;
+}
+
+const Auth: React.FC<AuthProps> = ({ session }) => {
+  const [username, setUsername] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (!username) return;
+
+    setLoading(true);
+
+    try {
+      await axios.post("/api/hello", {
+        username,
+      });
+      reloadSession();
+    } catch (error) {
+      console.log("onSubmit error", error);
+    }
+  };
+
+  const reloadSession = () => {
+    const event = new Event("visibilitychange");
+    document.dispatchEvent(event);
+    setLoading(false);
+  };
+
+  return (
+    <div className="flex w-full justify-center items-center border-2 border-blue-900">
+      <div className="flex flex-col items-center w-1/3">
+        {session ? (
+          <>
+            <p className="text-2xl text-gray-100">Create a Username</p>
+            <form
+              className="flex flex-col items-center w-full"
+              onSubmit={onSubmit}
+            >
+              <input
+                className="input input-bordered w-full max-w-xs my-6"
+                placeholder="Enter a username"
+                value={username}
+                onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                  setUsername(event.target.value)
+                }
+              />
+              <button
+                className={`btn btn-info w-1/3 ${true && "loading"}`}
+                type="submit"
+              >
+                Save
+              </button>
+            </form>
+          </>
+        ) : (
+          <>
+            <p className="text-3xl mb-10 text-gray-100">MessengerQL</p>
+            <button className="btn gap-4" onClick={() => signIn("google")}>
+              <img className="h-1/2" src="/images/googlelogo.png" />
+              Continue with Google
+            </button>
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+export default Auth;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,65 +2,24 @@ import axios from "axios";
 import type { NextPage } from "next";
 import { signIn, signOut, useSession } from "next-auth/react";
 import React, { useState } from "react";
+import Auth from "../components/Auth";
 import Spinner from "../components/globals/Spinner";
 
 const Home: NextPage = () => {
   const { data: session } = useSession();
-
-  const [username, setUsername] = useState("");
-  const [loading, setLoading] = useState(false);
-
-  const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    if (!username) return;
-
-    setLoading(true);
-
-    try {
-      await axios.post("/api/hello", {
-        username,
-      });
-      reloadSession();
-    } catch (error) {
-      console.log("onSubmit error", error);
-    }
-  };
-
-  const reloadSession = () => {
-    const event = new Event("visibilitychange");
-    document.dispatchEvent(event);
-    setLoading(false);
-  };
+  console.log("HERE IS SESSION", session);
 
   return (
-    <div className="flex justify-center">
-      <div className="flex flex-col border border-red-600">
-        {session?.user ? (
-          <>
-            <button onClick={() => signOut()}>Sign Out</button>
-            {session.user.username ? (
-              <div>Welcome {session.user.username}</div>
-            ) : (
-              <form onSubmit={onSubmit}>
-                <input
-                  placeholder="Enter a username"
-                  value={username}
-                  onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
-                    setUsername(event.target.value)
-                  }
-                />
-                <button type="submit">Save</button>
-                {loading && <span>LOADING</span>}
-              </form>
-            )}
-          </>
-        ) : (
-          <button onClick={() => signIn("google")}>Login With Google</button>
-        )}
-        <h1 className="text-3xl font-bold">Hello world!</h1>
-        <button className="btn btn-primary">Button</button>
-        <Spinner />
-      </div>
+    <div className="flex h-screen border border-red-500">
+      {session && session?.user?.username ? (
+        <div>
+          <button className="btn btn-primary" onClick={() => signOut()}>
+            Logout
+          </button>
+        </div>
+      ) : (
+        <Auth session={session} />
+      )}
     </div>
   );
 };
