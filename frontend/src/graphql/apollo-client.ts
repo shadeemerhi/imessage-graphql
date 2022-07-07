18b8f42f71111abe3a698f97dbc147285a0567e4
shadeemerhi <shadmerhi@gmail.com>
2022-07-07 08:45:50 -0600

    remove header configuration

--- frontend/src/graphql/apollo-client.ts
+++ frontend/src/graphql/apollo-client.ts
@@ -1,14 +1,9 @@
-import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
-import { setContext } from "@apollo/client/link/context";
+import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
 
 const link = createHttpLink({
   uri: "http://localhost:4000/graphql",
   credentials: "include",
 });
-
-// const authLink = setContext((request, prevContext) => ({
-//   headers: { authorization: "1234" },
-// }));
 
 const client = new ApolloClient({
   link,
