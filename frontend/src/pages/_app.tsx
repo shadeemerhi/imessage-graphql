32181bac459eabacbf74573851f884b291b043cc
shadeemerhi <shadmerhi@gmail.com>
2022-07-06 21:15:27 -0600

    create apollo client

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,16 +1,16 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
-import client from "../graphql/apollo-client";
 import { ApolloProvider } from "@apollo/client";
+import client from "../graphql/apollo-client";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    // <ApolloProvider client={client}>
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
-    // </ApolloProvider>
+    <ApolloProvider client={client}>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </ApolloProvider>
   );
 }
 
