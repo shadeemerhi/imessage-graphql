4f1fdba8ba0fcba0d429207799b4816fdbeb83b7
shadeemerhi <shadmerhi@gmail.com>
2022-07-16 13:37:32 -0600

    add responsive logic to chat components

--- frontend/src/components/Chat/index.tsx
+++ frontend/src/components/Chat/index.tsx
@@ -1,15 +1,16 @@
 import { Flex } from "@chakra-ui/react";
-import React from "react";
+import React, { useState } from "react";
 import Conversations from "./Conversations/Conversations";
 import Feed from "./Feed/Feed";
 
 interface ChatProps {}
 
 const Chat: React.FC<ChatProps> = () => {
+  const [convId, setConvId] = useState("");
   return (
-    <Flex height="100vh" border="1px solid red">
-      <Conversations />
-      <Feed />
+    <Flex height="100vh">
+      <Conversations convId={convId} setConvId={setConvId} />
+      <Feed convId={convId} setConvId={setConvId} />
     </Flex>
   );
 };
