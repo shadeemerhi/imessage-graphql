3c921edc63c022ec41efa1c636b49d97599109ed
shadeemerhi <shadmerhi@gmail.com>
2022-07-16 13:39:41 -0600

    remove unused imports

--- frontend/src/components/Chat/Feed/Feed.tsx
+++ frontend/src/components/Chat/Feed/Feed.tsx
@@ -1,11 +1,4 @@
-import {
-  Box,
-  Button,
-  Flex,
-  Slide,
-  Text,
-  useDisclosure,
-} from "@chakra-ui/react";
+import { Box, Button, Flex, Text } from "@chakra-ui/react";
 import { signOut } from "next-auth/react";
 import React from "react";
 import MessageInput from "./Input";
