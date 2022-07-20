8ab1e1c14813165a69319ad26f19e47c2188333f
shadeemerhi <shadmerhi@gmail.com>
2022-07-19 22:50:39 -0600

    install graphql-ws and configure ws transport

--- frontend/src/graphql/apollo-client.ts
+++ frontend/src/graphql/apollo-client.ts
@@ -1,13 +1,38 @@
-import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
+import { ApolloClient, HttpLink, InMemoryCache, split } from "@apollo/client";
+import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
+import { createClient } from "graphql-ws";
+import { getMainDefinition } from "@apollo/client/utilities";
 
-const link = createHttpLink({
-  uri: "http://localhost:4000/graphql",
+const wsLink =
+  typeof window !== "undefined"
+    ? new GraphQLWsLink(
+        createClient({
+          url: "ws://localhost:4000/graphql/subscriptions",
+        })
+      )
+    : null;
+
+const httpLink = new HttpLink({
+  uri: `http://localhost:4000/graphql`,
   credentials: "include",
 });
 
-const client = new ApolloClient({
+const link =
+  typeof window !== "undefined" && wsLink != null
+    ? split(
+        ({ query }) => {
+          const def = getMainDefinition(query);
+          return (
+            def.kind === "OperationDefinition" &&
+            def.operation === "subscription"
+          );
+        },
+        wsLink,
+        httpLink
+      )
+    : httpLink;
+
+export const client = new ApolloClient({
   link,
   cache: new InMemoryCache(),
 });
-
-export default client;
--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -4,7 +4,7 @@ import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
 import { Toaster } from "react-hot-toast";
 import { theme } from "../chakra/theme";
-import client from "../graphql/apollo-client";
+import { client } from "../graphql/apollo-client";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
