1d3457c67ecfcc131983c53c004636eb2a8afdbd
shadeemerhi <shadmerhi@gmail.com>
2022-07-23 09:52:30 -0600

    add onSendMessage function

--- frontend/src/components/Chat/Feed/Input.tsx
+++ frontend/src/components/Chat/Feed/Input.tsx
@@ -2,7 +2,8 @@ import { useMutation } from "@apollo/client";
 import { Box, Input } from "@chakra-ui/react";
 import { Session } from "next-auth";
 import { useSession } from "next-auth/react";
-import React from "react";
+import React, { useState } from "react";
+import toast from "react-hot-toast";
 import MessageOperations, {
   SendMessageVariables,
 } from "../../../graphql/operations/messages";
@@ -16,16 +17,38 @@ const MessageInput: React.FC<MessageInputProps> = ({
   session,
   conversationId,
 }) => {
+  const [messageBody, setMessageBody] = useState("");
+
   const [sendMessage] = useMutation<boolean, SendMessageVariables>(
     MessageOperations.Mutations.sendMessage
   );
 
-  const onSendMessage = async () => {};
+  const onSendMessage = async () => {
+    try {
+      const { id: senderId } = session.user;
+      const { data, errors } = await sendMessage({
+        variables: {
+          senderId,
+          conversationId,
+          body: messageBody,
+        },
+      });
+
+      if (!data || errors) {
+        throw new Error("Error sending message");
+      }
+    } catch (error: any) {
+      console.log("onSendMessage error", error);
+      toast.error(error?.message);
+    }
+  };
 
   return (
     <Box px={4} py={8} width="100%" flexGrow={1}>
       <form onSubmit={onSendMessage}>
         <Input
+          value={messageBody}
+          onChange={(event) => setMessageBody(event.target.value)}
           size="lg"
           placeholder="New message"
           color="whiteAlpha.900"
