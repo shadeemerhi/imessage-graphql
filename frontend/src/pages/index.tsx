7cf361284557d2a0eaad097d609d061b89fc146d
shadeemerhi <shadmerhi@gmail.com>
2022-09-24 08:41:15 -0600

    remove convId from url on logout

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,16 +1,25 @@
 import { Box } from "@chakra-ui/react";
 import type { NextPage, NextPageContext } from "next";
 import { getSession, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 import Auth from "../components/Auth/Auth";
 import Chat from "../components/Chat";
 
 const Home: NextPage = () => {
   const { data: session } = useSession();
+  const router = useRouter();
 
   const reloadSession = () => {
     const event = new Event("visibilitychange");
     document.dispatchEvent(event);
   };
+
+  useEffect(() => {
+    if (!session?.user && router.query.conversationId) {
+      router.replace(process.env.NEXT_PUBLIC_BASE_URL as string);
+    }
+  }, [session?.user, router.query]);
 
   return (
     <Box>
